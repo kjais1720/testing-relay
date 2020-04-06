638ca3f5876107aaf6496c6528c45354134c909d
Anuj Gupta <anuj.g@appointy.com>
2020-04-06 17:48:58 +0530

    feat: validation rules

--- src/utils/DesignationAddValidations.ts
+++ src/utils/DesignationAddValidations.ts
@@ -2,7 +2,8 @@ import * as yup from 'yup'
 import { t } from '@lingui/macro'
 
 const DesignationAddValidations = yup.object().shape({
-    name: yup.string().required(t`Required` as unknown as string),
+    name: yup.string().required(t`Required` as unknown as string).max(50, t`Max 50 characters allowed` as unknown as string),
+    description: yup.string().max(2000, t`Max 2000 characters allowed` as unknown as string),
 })
 
 export default DesignationAddValidations
