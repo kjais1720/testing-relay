2fe5d9e8eabd15a73e5b3c07d4c67fccf7dde744
Hariom Sharma <hariom@appointy.com>
2020-05-30 12:36:30 +0530

    fix: language

--- src/forms/DesignationAddFormComponent.tsx
+++ src/forms/DesignationAddFormComponent.tsx
@@ -1,35 +1,22 @@
-import { Plural, t, Trans } from '@lingui/macro'
+import { Trans } from '@lingui/macro'
 import { Box, Grid } from '@material-ui/core'
 import MenuItem from '@material-ui/core/MenuItem/MenuItem'
 import { Toggle } from '@saastack/components'
-import {  Form, Input, Select, Textarea } from '@saastack/forms'
-
+import { Form, Input, Select, Textarea } from '@saastack/forms'
 import { FormProps } from '@saastack/forms/types'
-import { FormikContextType } from 'formik/dist/types'
-
 import React from 'react'
 
 export interface Props<T extends ReadonlyArray<{}>> extends FormProps {
     roles: any,
-    isUpdate?:boolean
+    isUpdate?: boolean
 }
 
-const DesignationAddFormComponent = <T extends ReadonlyArray<{}> = any>({ roles, isUpdate,...props }: Props<T>) => {
+const DesignationAddFormComponent = <T extends ReadonlyArray<{}> = any>({ roles, isUpdate, ...props }: Props<T>) => {
     const [show, setShow] = React.useState(false)
-
-    const isDisabled = (id: string, roleIds: string[]) => {
-        const _role = roles.find((r: any) => r.id === id && r.isDefault && r.priority)
-        const _roles = roleIds.map(_r => roles.find((r: any) => r.id === _r)).filter(r => r.isDefault && r.priority)
-        if (_role) {
-            return _roles.find(_r => _role.priority > _r.priority)
-        }
-        return false
-    }
-
     return (
         <Form {...props}>
             <Input large name="name" label={<Trans>Title</Trans>} grid={{ xs: 12 }}/>
-            <Toggle label={isUpdate ? <Trans>Update Description</Trans>: <Trans>Add Description</Trans>} show={show} onShow={setShow}>
+            <Toggle label={isUpdate ? <Trans>Update Description</Trans> : <Trans>Add Description</Trans>} show={show} onShow={setShow}>
                 <Textarea grid={{ xs: 12 }} label={<Trans>Description</Trans>} name="description"/>
             </Toggle>
             <Grid item xs={12}>
