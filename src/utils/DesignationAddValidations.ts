5f08e8655655a06e9506ebe17e473d54861c2a56
Anuj Gupta <anuj.g@appointy.com>
2020-08-13 15:40:05 +0530

    fix: validations

--- src/utils/DesignationAddValidations.ts
+++ src/utils/DesignationAddValidations.ts
@@ -2,8 +2,8 @@ import * as yup from 'yup'
 import { t } from '@lingui/macro'
 
 const DesignationAddValidations = yup.object().shape({
-    name: yup.string().required(t`Required` as unknown as string).max(50, t`Max 50 characters allowed` as unknown as string),
-    description: yup.string().max(2000, t`Max 2000 characters allowed` as unknown as string),
+    name: yup.string().required(t`Required` as unknown as string).max(100, t`Max 100 characters allowed` as unknown as string),
+    description: yup.string().max(3000, t`Max 3000 characters allowed` as unknown as string),
 })
 
 export default DesignationAddValidations
